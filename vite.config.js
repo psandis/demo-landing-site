a61a773257014c0168ec790267dd0e2dfad605d9
Petri Sandholm <petri.sandholm@boxinom.com>
2026-03-20 15:20:50 +0100

    Update base path and package name to match new repo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/demo-landing-site/',
+  base: '/3d-landing-site/',
   server: {
     port: 3111,
   },
